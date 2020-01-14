ab88242dfbb8d5de5b32980310fcf3be5758ebe5
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-01-14 16:49:27 +0100

    adding new frames methods with explanation

--- src/com/spore/main/Game.java
+++ src/com/spore/main/Game.java
@@ -1,9 +1,21 @@
 package com.spore.main;
 
+import java.awt.*;
+
 // Główna klasa która będzie wszystko przechowywać, np. tu "narysujemy" nasz ekran i przechowamy metodę main z grą.
-public class Game {
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 3855241409607072348L; // Co to jest?
 
 
+    public synchronized void start () {
+
+    }
+
+    @Override // abstrakcyjna metoda z interejsu "Runnable"
+    public void run() {
+
+    }
 
     public static void main (String[] args) {
 
--- src/com/spore/main/Window.java
+++ src/com/spore/main/Window.java
@@ -10,12 +10,20 @@ public class Window extends Canvas {
 
     // Constructor containing also Game class because game will be inside window of c
     public Window (int height, int width, String title, Game game) {
-        JFrame frame = new JFrame(title); // Window title by using "JFrame"
+        JFrame frame = new JFrame(title); // Window by using "JFrame" z Java library
 
-        // EXPLANATION - LATER.
+        // EXPLANATION - LATER, for now - set maximum and minimum size of window
         frame.setPreferredSize(new Dimension(height, width));
         frame.setMaximumSize(new Dimension(height, width));
         frame.setMinimumSize(new Dimension(height, width));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // bez tego operacje nie zatrzymają się gdy wyłączymy gre
+        frame.setResizable(false); // Wyłączenie możliwości zmiany wielkości okna co stworzyło by problemy
+        frame.setLocationRelativeTo(null); // Nie wymagane ale pozwala włączyć okno na środku zamiast w górnym lewym rogu
+        frame.add(game); // Dodanie zawartości klasy "Game" do zawartości okna
+        frame.setVisible(true); // Aby można było wszystko widzieć - do dopytania
+        game.start() // Użycie metody z obiektu main
+
     }
 
 }
