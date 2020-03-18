6d5d7b474008a0b2b0d78c6d2d4b515f38e1e6fd
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-03-18 20:38:55 +0100

    GameObject constructor as class parameters setter

--- src/com/spore/main/GameObject.java
+++ src/com/spore/main/GameObject.java
@@ -4,4 +4,10 @@ public abstract class GameObject { //All objects like "coins","enemy","player" i
     protected int x, y; // chyba współrzędne
     protected ID id;
     protected int velX, velY;  // speed of moving into x and y direction
+
+    public GameObject (int x, int y, ID id) { // class constructor (set object parameters)
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
 }
