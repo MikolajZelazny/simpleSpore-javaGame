9eede80a1bd99fda99f92dfbb938e3a94d964d8e
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-04-03 21:25:17 +0200

    setters and getters positioned to another configuration

--- src/com/spore/main/GameObject.java
+++ src/com/spore/main/GameObject.java
@@ -7,26 +7,15 @@ public abstract class GameObject { //All objects like "coins","enemy","player" i
     protected ID id;
     protected int velX, velY;  // speed of moving into x and y direction
 
-    public GameObject (int x, int y, ID id) { // class constructor (set object parameters)
+    public GameObject(int x, int y, ID id) { // class constructor (set object parameters)
         this.x = x;
         this.y = y;
         this.id = id;
     }
 
     public abstract void tick();
+
     public abstract void render(Graphics g);
-
-    public void setX(int x) { // like constructor
-        this.x = x; //without x is not doing anything because we just setting parameter that we set.
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setId(ID id) {
-        this.id = id;
-    }
 
     public int getX() {
         return x;
@@ -39,4 +28,34 @@ public abstract class GameObject { //All objects like "coins","enemy","player" i
     public ID getId() {
         return id;
     }
+
+    public int getVelX() {
+        return velX;
+    }
+
+    public int getVelY() {
+        return velY;
+    }
+    // SETTERS
+
+    public void setX(int x) {// Like constructor
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
 }
+
