362ff892050b502d566ed37726e874f5cb5f684e
MikolajZelazny <MikolajZelazny.it@gmail.com>
2020-04-13 17:46:33 +0200

    setting key events

--- src/com/spore/main/KeyInput.java
+++ src/com/spore/main/KeyInput.java
@@ -6,12 +6,21 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter {
 
     private Handler handler;
+
     public KeyInput(Handler handler) { // Everything we put into handler constructor gonna input to this.
         this.handler=handler;
     }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode(); // get code of pressed keys on keyboard.
+
+        for (int i=0; i < handler.object.size(); i++) {
+            GameObject tempObject = handler.object.get(i);
+
+            if(tempObject.getId() == ID.Player) { //KEYBOARD SETTINGS
+                if (key == KeyEvent.VK_W) tempObject.setY(tempObject.getY()-1);
+            }
+        }
 
         System.out.println(key);
     }
